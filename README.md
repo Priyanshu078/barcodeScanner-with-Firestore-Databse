27d9348c463269480ea4d9aac06c18f1d67d7a81
Priyanshu078 <priyanshupaliwal078@gmail.com>
2022-02-28 18:38:04 +0530

    basic ui done

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# barcodeScanner-with-Firestore-Databse
-This is the application which is used used to scan the barcodes in the idcard of the students and save them in the database
+# barcodescanner
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/homePage.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(widget.title),
+        ),
+        body: Column(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 6,
+              width: MediaQuery.of(context).size.height / 6,
+            ),
+            const Align(
+              alignment: Alignment.center,
+              child: Padding(
+                padding: EdgeInsets.all(20),
+                child: Text(
+                  "Insight 2k22",
+                  style: TextStyle(
+                      color: Colors.redAccent,
+                      fontSize: 40,
+                      fontWeight: FontWeight.w400),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 8,
+              width: MediaQuery.of(context).size.height / 8,
+            ),
+            InkWell(
+              onTap: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text("Scan Pressed")));
+                scanBarCode();
+              },
+              child: Container(
+                height: MediaQuery.of(context).size.height / 4,
+                width: MediaQuery.of(context).size.height / 4,
+                decoration: BoxDecoration(
+                    color: Colors.red, borderRadius: BorderRadius.circular(10)),
+                child: const Center(
+                    child: Text(
+                  "Scan",
+                  style: TextStyle(color: Colors.white, fontSize: 40),
+                )),
+              ),
+            )
+          ],
+        ));
+  }
+
+  void scanBarCode() async {
+    String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+        "ff000000", "Cancel", true, ScanMode.BARCODE);
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(barcodeScanRes)));
+  }
+}
